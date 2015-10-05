e25c412154f45734e71f6a080a5acb1ba329fa80
Mikhail Valeyko <mikhail.valeyko@gmail.com>
2015-10-05 11:05:19 +0300

    Added repeatFunc

--- src/HomeWork9.hs
+++ src/HomeWork9.hs
@@ -2,5 +2,6 @@ module HomeWork9 where
 
 swapSymbols str = map (\c -> if c == '!' then '?' else if c == '?' then '!' else c) str
 
-multTable n = multTable' n []
-multTable' n acc = map (\i -> acc ++ (map (\j -> i * j) [1..n])) [1..n]
+multTable n = multTable' n [] where multTable' n acc = map (\i -> acc ++ (map (\j -> i * j) [1..n])) [1..n]
+
+repeatFunc f n | n == 1 = f | otherwise = f . repeatFunc f (n - 1)
