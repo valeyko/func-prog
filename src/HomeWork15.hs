390e5b9a31dcab29458c6e90e5596c6230b62438
Mikhail Valeyko <mikhail.valeyko@billing.ru>
2015-10-14 18:15:08 +0300

    task 2

--- src/HomeWork15.hs
+++ src/HomeWork15.hs
@@ -8,6 +8,10 @@ divs n i | i > d = 1
          | otherwise = if (mod n i == 0) then i + divs n (i + 1) else divs n (i + 1)
          where d = div n 2
 
+-- 2
+amicables n | (n == 1) = []
+            | otherwise = [(a, b) | a <- [1..n], let b = sumDiv a, a == sumDiv b, a < b]
+
 -- 3
 data Tree = Empty | Node Int Tree Tree
 foldTree f e (Node val l r) = f val resl resr
@@ -15,6 +19,9 @@ foldTree f e (Node val l r) = f val resl resr
                                     resr = foldTree f e r
 foldTree f e Empty = e
 sumSqr = foldTree (\v l r -> v ^ 2 + l + r) 0
+
+-- 4
+data Scheme a = P (Scheme a) (Scheme a) | S (Scheme a) (Scheme a) | R a
 
 -- 5
 ones = [ones' i | i <- [1..]]
