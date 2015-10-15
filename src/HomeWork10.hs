5a2b945ab089e95b0cba174fe2bdf0f50e1de36a
Mikhail Valeyko <mikhail.valeyko@gmail.com>
2015-10-15 16:59:58 +0300

    powerset

--- src/HomeWork10.hs
+++ src/HomeWork10.hs
@@ -3,3 +3,7 @@ module HomeWork10 where
 -- 1
 transpose xs | any (\x -> x == []) xs = []
              | otherwise = (map head xs) : transpose (map tail xs)
+
+-- 2
+powerset [] = [[]]
+powerset (x:xs) = powerset xs ++ map (x:) (powerset xs)
